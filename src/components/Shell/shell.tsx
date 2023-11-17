4f9cc5bac74ea6e722a3effc6fa5b74a6b04f174
Liam Mahoney <liammahoney96@gmail.com>
2023-11-17 14:48:59 -0600

    implements shell - navbar and main content container

--- src/components/Shell/shell.tsx
+++ src/components/Shell/shell.tsx
@@ -10,7 +10,7 @@ type Props = {
     children: React.ReactNode
 }
 
-export default function Shell({ children }:Props) {
+export default function Shell({ children }: Props) {
     const [mobileOpen, setMobileOpen] = useState(false);
 
     const handleBurgerClick = (() => {
@@ -18,18 +18,33 @@ export default function Shell({ children }:Props) {
     });
 
     return (
-        <div mobile-open={`${mobileOpen}`} className={styles.shellContainer}>
-            <div className={styles.navbar}>
-                <div className={styles.navbarRight}>
-                    <span>Liam Mahoney</span>
-                </div>
-                <div className={styles.navbarCenter}>
-                    <Button variant="ghost">Home</Button>
-                    <Button variant="ghost">Blog</Button>
-                </div>
-                <div className={styles.navbarLeft}>
-                    <span className={styles.desktopRight}>
-                        {/* TODO: add links */}
+
+        <div className={styles.top_container} mobile-menu-open={`${mobileOpen}`}>
+            {/* gave up on trying to fix the following:
+                - disable scroll behind mobile navbar. either causes:
+                    - layout shift
+                    - scroll bar to appear behind navbar
+                - safari mobile scrolling down causes URL bar to dissapear / shrink, which causes navbar to be smaller than allowable space for a ~ second
+
+                scrollbar only being on content_container might be OK? issue above is the scrollbar goes behind the navbar, but if the scrollbar only went up to the navbar maybe this would help my issue?
+            */}
+            <nav className={styles.navbar}>
+                <div className={styles.navbar_center_container}>
+                    <div className={styles.navbar_center_left}>
+                        <p className={styles.logo}>Liam Mahoney</p>
+                    </div>
+                    <div className={styles.navbar_center_center}>
+                        <Button variant="ghost">
+                            Home
+                        </Button>
+                        <Button variant="ghost">
+                            Projects
+                        </Button>
+                        <Button variant="ghost"> 
+                            Posts
+                        </Button>
+                    </div>
+                    <div className={styles.navbar_center_right}>
                         <Button icon variant="ghost">
                             <LinkedinLogo size={16} weight="bold" />
                         </Button>
@@ -37,37 +52,43 @@ export default function Shell({ children }:Props) {
                             <GithubLogo size={16} weight="bold" />
                         </Button>
                         <ThemeToggle variant="ghost" />
-                    </span>
-                    <span className={styles.mobileRight}>
+                    </div>
+                    <div className={styles.navbar_center_right_mobile}>
                         <Hamburger 
-                            size={20} 
+                            size={20}
                             toggle={handleBurgerClick}
                             toggled={mobileOpen}
                         />
-                    </span>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.contentContainer}>
-                {children}
-            </div>
-            <div className={styles.mobileMenu}>
-                <div className={styles.mobileLinkContainer}>
-                    <Button variant="ghost" fullWidth size="xl">Home</Button>
-                    <Button variant="ghost" fullWidth size="xl">Blog</Button>
-                </div>
-                <div className={styles.mobileIconContainer}>
+            </nav>
+            <div mobile-menu-open={`${mobileOpen}`} className={styles.mobile_nav}>
+                <div className={styles.mobile_nav_center_container}>
                     {/* TODO: add links */}
-                    {/* TODO: figure out best way to increase text size. way too small on mobile */}
-                    {/* FIXME: opening mobile nav and closing results in page getting reset to top.. not ideal */}
-                    <Button icon variant="ghost" size="lg">
+                    <Button fullWidth size="xl" variant="ghost">
+                        <span className={styles.mobile_button_text}>Home</span>
+                    </Button>
+                    <Button fullWidth size="xl" variant="ghost">
+                        <span className={styles.mobile_button_text}>Projects</span>
+                    </Button>
+                    <Button fullWidth size="xl" variant="ghost"> 
+                        <span className={styles.mobile_button_text}>Posts</span>
+                    </Button>
+                </div>
+                <div className={styles.mobile_nav_bottom_container}>
+                    <Button icon size="lg" variant="ghost">
                         <LinkedinLogo size={20} weight="bold" />
                     </Button>
-                    <Button icon variant="ghost" size="lg">
+                    <Button icon size="lg" variant="ghost"> 
                         <GithubLogo size={20} weight="bold" />
                     </Button>
-                    <ThemeToggle variant="ghost" size="lg"/>
+                    <ThemeToggle size="lg" variant="ghost" />
                 </div>
             </div>
+            <main className={styles.content_container}>
+                {children}
+            </main>
         </div>
+
     )
 }
